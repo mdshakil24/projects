5b05c175fff60073bbd4e3910e809823217d9519
shakilshd23 <shakilshd23@gmail.com>
2024-01-04 17:23:10 +0600

    first commit

---
+++ assets/js/custom.js
@@ -0,0 +1,12 @@
+ jQuery(document).ready(function() {
+
+
+		/*********** Nav menu ***************/
+		jQuery('.nav-icon').click(function(){
+			jQuery('.main-menu-area').toggleClass('canvas-menu');
+			return false;
+		});
+
+
+
+});
